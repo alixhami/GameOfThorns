cb89779795e34bb52f45d4309f31a2022f12c5cd
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-06 12:50:23 -0700

    adds drunk walk

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Animator))]
+public class Movement : MonoBehaviour {
+
+  Animator anim;
+
+  void Awake () {
+    anim = GetComponent<Animator> ();
+  }
+
+  void Update() {
+    Move();
+    // react
+  }
+
+  void Move() {
+    anim.SetFloat("Drunk", 1f);
+  }
+}
