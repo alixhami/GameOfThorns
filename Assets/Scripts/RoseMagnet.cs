289dd2447c4dfabcd94e2a4b9460bba770788467
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-07 11:13:13 -0700

    adds rose sticking script to spawnees

---
+++ Assets/Scripts/RoseMagnet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoseMagnet : MonoBehaviour {
+
+  void OnCollisionEnter (Collision c) {
+    if (c.gameObject.tag == "Rose") {
+      c.transform.SetParent(transform);
+      c.transform.GetComponent<Rigidbody>().isKinematic = true;
+    }
+  }
+
+}
