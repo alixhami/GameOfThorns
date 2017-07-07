94a3b9e8e3fdc97e25382473ea73fbf1d140416a
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-07 10:40:35 -0700

    creates spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+  public GameObject[] spawnees;
+  int randomInt;
+
+  void Start () {
+    InvokeRepeating("SpawnRandom", 1.0f, 2.0f);
+  }
+
+  void SpawnRandom () {
+    randomInt = Random.Range (0, spawnees.Length);
+
+    // Set random position on x axis, based on position of spawn origin
+    Vector3 randomPos = transform.position;
+    randomPos.x = Random.Range (-5f, 5f);
+
+    GameObject newSpawn = Instantiate (spawnees [randomInt], randomPos, transform.rotation)as GameObject;
+  }
+}
