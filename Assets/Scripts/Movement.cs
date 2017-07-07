d4364a5d9b54b77b68fcd228ef2bbc6008a2fb47
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-07 13:07:16 -0700

    characters do fist pump when they get a rose

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,20 +8,25 @@ using UnityEngine;
 [RequireComponent(typeof(CapsuleCollider))]
 public class Movement : MonoBehaviour {
 
-  Animator anim;
-  Rigidbody rb;
+	Animator anim;
+	Rigidbody rb;
 
+	void OnCollisionEnter (Collision c) {
+		if (c.gameObject.tag == "Rose") {
+			anim.SetTrigger ("getRose");
+		}
+	}
 
-  void Awake () {
-    anim = GetComponent<Animator> ();
-  }
+	void Awake () {
+		anim = GetComponent<Animator> ();
+	}
 
-  void Update() {
-    Move();
-    // react
-  }
+	void Update() {
+		Move();
+		// react
+	}
 
-  void Move() {
-    anim.SetFloat("Forward", 1f);
-  }
+	void Move() {
+		anim.SetFloat("Forward", 1f);
+	}
 }
