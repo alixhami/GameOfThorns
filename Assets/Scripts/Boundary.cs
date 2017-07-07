54270767656e8dea6060b7983c35d56cde7fb034
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-07 10:29:40 -0700

    adds boundary

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour {
+
+  void OnTriggerExit(Collider other) {
+    Destroy(other.gameObject);
+  }
+
+}
