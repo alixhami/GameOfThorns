258af6b6b9e909d7e825e5d9f9dfcb19e25feed0
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-09 14:00:22 -0700

    randomizes character navmeshagent accuracy so the characters aren't single file

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,28 +11,29 @@ public class Movement : MonoBehaviour {
 
 	Animator anim;
 	Rigidbody rb;
-  string[] tags = new string[]{"TargetCharacter", "EnemyCharacter"};
-  public GameObject[] waypoints;
-  int currentWP = 0;
-  float accuracyWP = 3.0f;
-  NavMeshAgent agent;
+	string[] tags = new string[]{"TargetCharacter", "EnemyCharacter"};
+	public GameObject[] waypoints;
+	int currentWP = 0;
+	float accuracyWP;
+	NavMeshAgent agent;
 
 	void Awake () {
-    anim = GetComponent<Animator> ();
-    tag = tags [Random.Range (0, tags.Length)];
+		anim = GetComponent<Animator> ();
+		tag = tags [Random.Range (0, tags.Length)];
+		accuracyWP = Random.Range (2.0f, 5.0f);
 	}
 
-  void Start () {
-    agent = GetComponent<NavMeshAgent> ();
-    Move();
-  }
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		Move();
+	}
 
-  void Update () {
-    if (Vector3.Distance (waypoints [currentWP].transform.position, transform.position) < accuracyWP) {
-      currentWP++;
-    }
-    agent.destination = waypoints[currentWP].transform.position;
-  }
+	void Update () {
+		if (Vector3.Distance (waypoints [currentWP].transform.position, transform.position) < accuracyWP) {
+	  		currentWP++;
+		}
+		agent.destination = waypoints[currentWP].transform.position;
+	}
 
 	void Move() {
     
@@ -44,16 +45,16 @@ public class Movement : MonoBehaviour {
     
 	}
 
-  void OnAnimatorMove () {
-    Vector3 position = anim.rootPosition;
-    position.y = agent.nextPosition.y;
-    transform.position = position;
-  }
+	void OnAnimatorMove () {
+		Vector3 position = anim.rootPosition;
+		position.y = agent.nextPosition.y;
+		transform.position = position;
+	}
 
-  void OnCollisionEnter (Collision c) {
-    if (c.gameObject.tag == "Rose") {
-      anim.SetTrigger ("getRose");
-      anim.SetBool ("hasRose", true);
-    }
-  }
+	void OnCollisionEnter (Collision c) {
+		if (c.gameObject.tag == "Rose") {
+	  		anim.SetTrigger ("getRose");
+	  		anim.SetBool ("hasRose", true);
+		}
+	}
 }
