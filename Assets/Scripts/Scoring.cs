9321509cbf34790c6ce00e7d8b49c14f85379ac9
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-09 15:44:27 -0700

    sets up scoreboard update capability

---
+++ Assets/Scripts/Scoring.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoring : MonoBehaviour {
+
+	int positivePoints;
+	int negativePoints;
+	public Text positivePointDisplay;
+	public Text negativePointDisplay;
+
+	void Update () {
+		positivePointDisplay.text = positivePoints.ToString();
+		negativePointDisplay.text = negativePoints.ToString();
+	}
+}
