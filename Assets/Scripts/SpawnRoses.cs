4ffba9c152dabc9fb00142ef8ddc852627053020
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-12 14:50:22 -0700

    adds rose spawner and switches characters to man in suit

---
+++ Assets/Scripts/SpawnRoses.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRoses : MonoBehaviour {
+
+	public GameObject rose;
+	private SteamVR_TrackedController controller;
+
+	void Start () {
+		controller = GetComponentInParent<SteamVR_TrackedController> ();
+		controller.TriggerClicked += SpawnRose;
+	}
+
+	void SpawnRose (object sender, ClickedEventArgs e) {
+		//transform == gameObject.transform == gameObject.GetComponent<Transform>()
+
+		if (rose) {
+			GameObject newRose = Instantiate(rose, transform.position, transform.rotation)as GameObject;
+			newRose.transform.SetParent(transform);
+		}
+	}
+}
