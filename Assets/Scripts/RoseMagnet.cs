ed54e55f40275d64276c6cfbd26baea1d279aaf3
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-14 20:56:55 -0700

    rose floats to appropriate pinning position

--- Assets/Scripts/RoseMagnet.cs
+++ Assets/Scripts/RoseMagnet.cs
@@ -5,27 +5,28 @@ using UnityEngine;
 public class RoseMagnet : MonoBehaviour {
 
 	public GameObject character;
-  public Transform roseTarget;
-  public float roseSpeed = 0.1f;
+	public Transform roseTarget;
+	public float roseSpeed = 0.1f;
 
 	void OnTriggerEnter (Collider c) {
 
 		if (c.gameObject.tag == "Rose" && !character.GetComponent<Movement>().receivedRose ) {
   			c.transform.SetParent(transform);
   			c.transform.GetComponent<Rigidbody>().isKinematic = true;
-        StopAllCoroutines();
-        StartCoroutine(moveRoseToTarget(c.transform));
+			StopAllCoroutines();
+			StartCoroutine(moveRoseToTarget(c.transform));
 
-			  character.GetComponent<Movement> ().GetRose ();
-  	 }
-  }
+			character.GetComponent<Movement> ().GetRose ();
+		}
+	}
 
-  IEnumerator moveRoseToTarget (Transform roseTransform) {
-    for (int i = 0; i < 1000; i++) {
-      roseTransform.position = Vector3.Lerp (roseTransform.position, roseTarget.position, roseSpeed);
-      yield return new WaitForSeconds (0.01f);
-    }
-  }
+	IEnumerator moveRoseToTarget (Transform roseTransform) {
+		for (int i = 0; i < 1000; i++) {
+			roseTransform.position = Vector3.Lerp (roseTransform.position, roseTarget.position, roseSpeed);
+			roseTransform.rotation = Quaternion.Lerp (roseTransform.rotation, roseTarget.rotation, roseSpeed);
+			yield return new WaitForSeconds (0.01f);
+		}
+	}
 
 
 
