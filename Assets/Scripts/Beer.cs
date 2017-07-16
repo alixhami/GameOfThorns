7378d64c815e4f8696bc25de57709dddbb3dfc24
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-15 17:16:02 -0700

    adds sounds for objects hitting environment

---
+++ Assets/Scripts/Beer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beer : MonoBehaviour {
+
+	public AudioClip hitGroundSound;
+	public AudioClip hitBeerSound;
+	AudioSource audioSource;
+
+	bool hasDropped = false;
+
+	void Awake() {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	void OnCollisionEnter (Collision other) {
+		if (other.gameObject.tag == "Beer") {
+			audioSource.PlayOneShot (hitBeerSound);
+		} else if (other.gameObject.isStatic && !hasDropped) {
+			audioSource.PlayOneShot (hitGroundSound);
+			hasDropped = true;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Rose.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rose : MonoBehaviour {
+
+	public AudioClip hitSound;
+	AudioSource audioSource;
+
+	void Awake() {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	void OnCollisionEnter (Collision other) {
+		if (other.gameObject.isStatic) {
+			audioSource.PlayOneShot (hitSound);
+		}
+	}
+}
