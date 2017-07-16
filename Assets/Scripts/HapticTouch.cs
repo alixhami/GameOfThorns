5f98b8b3a081b4497d1adee038a5e6c3c29fe8f9
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-15 18:05:06 -0700

    adds haptic touch to controllers

---
+++ Assets/Scripts/HapticTouch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HapticTouch : MonoBehaviour {
+
+	public bool hapticFlag = false;
+	private SteamVR_TrackedObject trackedObj;
+	private SteamVR_Controller.Device device;
+
+	void Awake () {
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+	}
+
+	void Update () {
+		device = SteamVR_Controller.Input ((int)trackedObj.index);
+		if (hapticFlag) {
+			device.TriggerHapticPulse (500);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag != "Beer" && other.gameObject.tag != "Rose") {
+			hapticFlag = true;	
+		}
+	}
+
+	void OnTriggerExit() {
+		hapticFlag = false;
+	}
+}
