1f871aadb94d2135acb34d18613ef1a88a8ee434
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-17 17:00:56 -0700

    throw rose through menu to start game

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+  public GameObject limo;
+
+  void Start () {
+    // Invoke("StartGame", 5f); 
+  }
+
+  void OnTriggerEnter (Collider other) {
+    if (other.gameObject.tag == "Rose") {
+      StartGame(); 
+    }
+  }
+
+  void StartGame () {
+    gameObject.SetActive(false);
+    limo.GetComponent<Animator>().SetTrigger("Arrive");
+  }
+}
