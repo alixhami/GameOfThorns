73a2c1e473ba857da699303822c4cdb518562eb9
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-18 11:23:46 -0700

    WIP player alerts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+  public Scoring scoring;
+  public PlayerAlerts playerAlerts;
+
+  int villainsSnuckInCount;
+  int villainsWithRosesCount;
+  int goodGuysWithRosesCount;
+
+  public void villainSneaksIn () {
+
+  }
+}
---
+++ Assets/Scripts/PlayerAlerts.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAlerts : MonoBehaviour {
+
+  Text text;
+  Color positiveAlertColor = new Color(201, 168, 34, 255);
+  Color negativeAlertColor = new Color(154, 38, 55, 255);
+
+  void Awake () {
+    text = GetComponent<Text>();
+  }
+
+  public void displayPositiveAlert(string message) {
+    text.color = positiveAlertColor;
+    text.text = message;
+  }
+
+  public void displayNegativeAlert(string message) {
+    text.color = negativeAlertColor;
+    text.text = message;
+  }
+
+
+}
