375fe4efd7fc20c54f8f1482ba6dc47050057798
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-24 13:14:49 -0700

    adds music fader for target practice

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,9 +14,19 @@ public class GameManager : MonoBehaviour {
   public bool playingGame = false;
 
 	public AudioSource music;
+  float maxMusicVolume = 0.08f;
+  float musicFadeSpeed = 0.01f;
+  public AudioClip seriousMusic;
+  public AudioClip hopefulMusic;
 
   public GameObject gameSelectPillars;
   public GameCard[] gameCards;
+
+  void Awake () {
+    music.volume = maxMusicVolume;
+    music.clip = hopefulMusic;
+    music.Play();
+  }
 
   void Start() {
     foreach (var gameCard in gameCards) {
@@ -27,22 +37,55 @@ public class GameManager : MonoBehaviour {
     limoGame.GameOver += GameOver;
   }
 
-  private void GameCard_TriggerGame(string gameName) {
+  private void GameCard_TriggerGame (string gameName) {
     playingGame = true;
-    switch(gameName) {
+    switch (gameName) {
       case "LimoTimed":
+        ChangeMusic(hopefulMusic);
         limoGame.Play (false);
         break;
       case "LimoSurvival":
+        ChangeMusic(hopefulMusic);
         limoGame.Play (true);
         break;
       case "TargetPractice":
+        ChangeMusic(seriousMusic);
         limoGame.Hide ();
         targetPractice.Play();
         break;
     }
     gameSelectPillars.SetActive(false);
     menu.HideMenus();
+  }
+
+  void ChangeMusic (AudioClip track) {
+    if (music.clip.name != track.name) {
+      StartCoroutine(ChangeTracks(track));
+    }
+  }
+
+  IEnumerator ChangeTracks (AudioClip track) {
+    float audioVolume = music.volume;
+
+    while (music.volume >= musicFadeSpeed) {
+      audioVolume -= musicFadeSpeed;
+      music.volume = audioVolume;
+      yield return new WaitForSeconds(1f);
+    }
+
+    music.Stop();
+
+    music.clip = track;
+
+    music.Play();
+    music.volume = 0.01f;
+    audioVolume = music.volume;
+
+    while (music.volume < maxMusicVolume) {
+      audioVolume += musicFadeSpeed;
+      music.volume = audioVolume;
+      yield return new WaitForSeconds(1f);
+    }
   }
 
   public void ShowInstructions () {
